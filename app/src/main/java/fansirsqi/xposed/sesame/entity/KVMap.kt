eba2b2a8ece2f45657f3a33f86b2b1aadaca6b52
byseven <2104898527@qq.com>
2025-08-05 15:34:13 +0800

    refactor: 修改KVMap构造函数和属性初始化方式

--- app/src/main/java/fansirsqi/xposed/sesame/entity/KVMap.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/entity/KVMap.kt
@@ -3,18 +3,10 @@ package fansirsqi.xposed.sesame.entity
 import java.io.Serial
 import java.io.Serializable
 
-open class KVMap<K, V> protected constructor() : Serializable {
+open class KVMap<K, V> (var key: K, var value: V) : Serializable {
 
     companion object {
         @Serial
         const val serialVersionUID: Long = 1L
     }
-
-    constructor(key: K, value: V) : this() {
-        this.key = key
-        this.value = value
-    }
-
-    var key: K? = null
-    var value: V? = null
 }
