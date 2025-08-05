d45d6ac084ba9b7e1324d906bd05abbb79af7400
byseven <2104898527@qq.com>
2025-08-05 15:36:49 +0800

    demosetting

--- app/src/main/java/fansirsqi/xposed/sesame/newui/Demo.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/newui/Demo.kt
@@ -3,7 +3,6 @@ package fansirsqi.xposed.sesame.newui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
