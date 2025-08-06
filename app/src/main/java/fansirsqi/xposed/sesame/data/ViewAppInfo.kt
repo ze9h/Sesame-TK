b33182399b41094c0940f33179d125f40fafc18f
byseven <2104898527@qq.com>
2025-08-06 19:08:32 +0800

    refactor(core): 重构初始化逻辑并更新标识符名称

--- app/src/main/java/fansirsqi/xposed/sesame/data/ViewAppInfo.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/data/ViewAppInfo.kt
@@ -2,10 +2,12 @@ package fansirsqi.xposed.sesame.data
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.provider.Settings
 import fansirsqi.xposed.sesame.BuildConfig
 import fansirsqi.xposed.sesame.R
+import fansirsqi.xposed.sesame.newutil.MMKVSettingsManager
 import fansirsqi.xposed.sesame.util.Log
+import java.util.UUID
+
 
 @SuppressLint("StaticFieldLeak")
 object ViewAppInfo {
@@ -15,7 +17,7 @@ object ViewAppInfo {
     var appVersion: String = ""
     var appBuildTarget: String = ""
     var appBuildNumber: String = ""
-    var androidId: String = ""
+    var verifyId: String = ""
     var veriftag: Boolean = false
 
     @SuppressLint("HardwareIds")
@@ -54,10 +56,14 @@ object ViewAppInfo {
     fun init(context: Context) {
         if (ViewAppInfo.context == null) {
             ViewAppInfo.context = context
+            MMKVSettingsManager.init(context)
+            verifyId = MMKVSettingsManager.mmkv.decodeString("verify").takeIf { !it.isNullOrEmpty() }
+                ?: UUID.randomUUID().toString().replace("-", "").also {
+                    MMKVSettingsManager.mmkv.encode("verify", it)
+                }
             appBuildNumber = BuildConfig.VERSION_CODE.toString()
             appTitle = context.getString(R.string.app_name) //+ BuildConfig.VERSION_NAME
             appBuildTarget = BuildConfig.BUILD_DATE + " " + BuildConfig.BUILD_TIME + " ⏰"
-            androidId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
             try {
                 appVersion = "${BuildConfig.VERSION_NAME} " + emojiList.random()
             } catch (e: Exception) {
